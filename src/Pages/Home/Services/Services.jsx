54a9b34a1ce54295fbbae5d1eeda4252954b1583
jerin-arpa <ismatjerrin3@gmail.com>
2023-10-27 03:34:56 +0600

    display service data

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
+
 /* eslint-disable react/no-unescaped-entities */
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, []);
+
     return (
-        <div>
+        <div className="mb-20">
             <div className="flex justify-center">
-                <div className="w-1/2 mb-10">
+                <div className="w-1/2 mb-5">
                     <p className='text-[#FF3811] font-bold pb-4 text-center'>Service</p>
                     <h1 className="text-5xl font-bold  text-center">Our Service Area</h1>
                     <p className="py-6 text-center">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
                 </div>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                {
+                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                }
             </div>
         </div>
     );
